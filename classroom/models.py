3cba3e1c78a9e2712fc701c4b8dd6786cea719ec
suryaravikumar-pixer <suryaravikumar@gmail.com>
2021-06-15 16:12:46 +0530

    class and students methods created

---
+++ classroom/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+
+class Student(models.Model):
+    """Model definition for student."""
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    admission_number = models.IntegerField(unique=True)
+
+    is_qualified = models.BooleanField(default=False)
+    average_score = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return self.first_name
+    
+    def get_grade(self):
+        if self.grade < 40:
+            return "Fail"
+        elif 40 < self.grade < 70:
+            return "Pass"
+        elif 70 < self.grade <100:
+            return "Out standing"
+        else:
+            return "Error"
+
+class Classroom(models.Model):
+    name = models.CharField(max_length=120)
+    student_capacity = models.IntegerField()
+    students = models.ManyToManyField("classroom.Student")
+
+    def __str__(self):
+        return self.name
